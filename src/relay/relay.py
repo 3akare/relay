4d82946e3325ddcb216f4d1c0a906a49710074a5
3akare <bakaredavid007@gmail.com>
2025-05-30 13:45:58 +0100

    feat [relay]: list dependencies

--- src/relay/relay.py
+++ src/relay/relay.py
@@ -1,11 +1,18 @@
-#!/usr/bin/env python3
 import argparse
 try:
     from relay.constants import RELAY_VERSION
-    from relay.commands import run_new, run_build, run_run, run_clean, run_install_command, run_update, run_remove, add_dependency_to_manifest
+    from relay.commands import (
+        run_new, run_build, run_run, run_clean,
+        run_install_command, run_list_dependencies,
+        run_remove_dependency, add_dependency_to_manifest
+    )
 except ModuleNotFoundError:
     from constants import RELAY_VERSION
-    from commands import run_new, run_build, run_run, run_clean, run_install_command, run_update, run_remove, add_dependency_to_manifest
+    from commands import (
+        run_new, run_build, run_run, run_clean,
+        run_install_command, run_list_dependencies,
+        run_remove_dependency, add_dependency_to_manifest
+    )
 except Exception:
     pass
 
@@ -14,34 +21,27 @@ def main():
         description="C/C++ package manager",
     )
 
-    # GLOBAL OPTIONS
-    # version
     parser.add_argument(
         "--version", "-V",
         action="version",
         version=f"relay {RELAY_VERSION}",
         help="Print version info and exit"
     )
-
-    # toolchain
     parser.add_argument(
         "--toolchain",
         help="Specify the build toolchain triplet (e.g., x64-windows, x64-linux). Defaults to VCPKG_DEFAULT_TRIPLET environment variable or guesses based on OS."
     )
-
-    # verbose
     parser.add_argument(
         "-v", "--verbose",
         action="store_true",
         help="Use verbose output"
     )
 
-    # COMMANDS
-    # new
     subparsers = parser.add_subparsers(
         dest="command",
         help="Available commands"
     )
+
     new_parser = subparsers.add_parser(
         "new",
         help="Create a new relay package"
@@ -52,15 +52,13 @@ def main():
     )
     new_parser.set_defaults(func=run_new)
 
-    # build
     build_parser = subparsers.add_parser(
         "build",
-         aliases=["b"],
+        aliases=["b"],
         help="Compile the current package"
     )
     build_parser.set_defaults(func=run_build)
 
-    # run
     run_parser = subparsers.add_parser(
         "run",
         aliases=["r"],
@@ -68,7 +66,6 @@ def main():
     )
     run_parser.set_defaults(func=run_run)
 
-    # install
     install_parser = subparsers.add_parser(
         "install",
         aliases=["i"],
@@ -76,7 +73,6 @@ def main():
     )
     install_parser.set_defaults(func=run_install_command)
 
-    # add
     add_parser = subparsers.add_parser(
         "add",
         help="Name of the dependency to add (e.g., 'fmt', 'zlib')"
@@ -87,7 +83,6 @@ def main():
     )
     add_parser.set_defaults(func=add_dependency_to_manifest)
 
-    # remove
     remove_parser = subparsers.add_parser(
         "remove",
         aliases=["rm"],
@@ -97,20 +92,15 @@ def main():
         "dependency_name",
         help="Name of the dependency to remove"
     )
-    remove_parser.set_defaults(func=run_remove)
+    remove_parser.set_defaults(func=run_remove_dependency)
 
-    # update
-    update_parser = subparsers.add_parser(
-        "update",
-        help="Update project dependencies"
+    list_parser = subparsers.add_parser(
+        "list",
+        aliases=["l"],
+        help="List project dependencies"
     )
-    update_parser.add_argument(
-        "dependency_name",
-        help="Name of the dependency to update"
-    )
-    update_parser.set_defaults(func=run_update)
+    list_parser.set_defaults(func=run_list_dependencies)
 
-     # clean
     clean_parser = subparsers.add_parser(
         "clean",
         help="Remove build artifacts and cached files"
@@ -118,7 +108,7 @@ def main():
     clean_parser.set_defaults(func=run_clean)
 
     args = parser.parse_args()
-    if (hasattr(args, 'func')):
+    if hasattr(args, 'func'):
         args.func(args)
     else:
         parser.print_help()
